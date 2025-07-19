4458318d5de6071d882a57b8e43c07b5d3275cdc
Bommu Saranya <161605690+saranyareddy221@users.noreply.github.com>
2025-07-19 18:18:45 +0530

    Add files via upload

---
+++ week 5 hands-on solutions/2. Microservices with API gateway/Creating Microservices for account/AccountController.java
@@ -0,0 +1,11 @@
+package com.cognizant.account.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AccountController {
+    @GetMapping("/accounts/{number}")
+    public String getAccountDetails(@PathVariable String number) {
+        return "{ number: \"" + number + "\", type: \"savings\", balance: 234343 }";
+    }
+}
